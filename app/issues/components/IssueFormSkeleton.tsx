7ca22d71650382594ce0f4c50026f5a1da2eebcc
Ziyad Hasen <hziyad933@gmail.com>
2024-09-24 16:26:48 +0300

    improve the loading experience

--- app/issues/components/IssueFormSkeleton.tsx
+++ app/issues/components/IssueFormSkeleton.tsx
@@ -1,15 +1,16 @@
+'use client';
 import { Box } from '@radix-ui/themes';
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
-const NewIssueLoading = () => {
+const IssueFormSkeleton = () => {
   return (
     <Box className='max-w-xl'>
-      <Skeleton></Skeleton>
+      <Skeleton height='2rem'></Skeleton>
       <Skeleton height='20rem'></Skeleton>
     </Box>
   );
 };
 
-export default NewIssueLoading;
+export default IssueFormSkeleton;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import IssueForm from '../components/IssueForm';
-
+import dynamic from 'next/dynamic';
+import IssueFormSkeleton from '../components/IssueFormSkeleton';
+const IssueForm = dynamic(() => import('@/app/issues/components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 const NewIssueForm = () => {
   return (
     <>
