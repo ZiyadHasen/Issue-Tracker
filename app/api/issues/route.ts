3eb0dd3749ce688b36dd29d117c659c5714178a8
Ziyad Hasen <hziyad933@gmail.com>
2024-09-26 11:02:19 +0300

    delete issue api and dialog done

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
-import { createIssueSchema } from '../../validationSchema';
+import { IssueSchema } from '../../validationSchema';
 
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = IssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,36 @@
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import React from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color='red'>Delete Issue</Button>;
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            are you sure you want to delete this issue , this work cant be
+            undone{' '}
+          </AlertDialog.Description>
+          <Flex gap='3' mt='4' justify='end'>
+            <AlertDialog.Cancel>
+              <Button variant='soft' color='gray'>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant='solid' color='red'>
+                Confirm Deletion
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      ;
+    </>
+  );
 };
 
 export default DeleteIssueButton;
